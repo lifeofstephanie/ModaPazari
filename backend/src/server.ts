f91a2065b96d8ce8064704ecb5b71e41d4f702bf
Lifeofstephanie <stephanieanyanwu605@gmail.com>
2026-01-11 09:12:12 +0100

    'done'

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 const MONGO_URI =
   process.env.MONGO_URI ||
   "mongodb+srv://stephanieanyanwu605_db_user:ciEhwT2C8XhlSyuy@modapazari.jgdqtzw.mongodb.net/?appName=ModaPazari";
@@ -11,7 +11,7 @@ mongoose
   .then(() => {
     console.log(`connected`);
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`connected to ${PORT}`);
     });
   })
