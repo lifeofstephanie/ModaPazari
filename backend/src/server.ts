39ab4142577d2cc1faf805034e32250ca7c8ea69
Lifeofstephanie <stephanieanyanwu605@gmail.com>
2026-01-11 07:45:16 +0100

    'backendupdate'

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,7 @@ import { app } from "./app";
 const PORT = process.env.PORT || 5000;
 const MONGO_URI =
   process.env.MONGO_URI ||
-  "mongodb+srv://stephanieanyanwu605_db_user:ciEhwT2C8XhlSyuy@modapazari.jgdqtzw.mongodb.net/modapazri";
+  "mongodb+srv://stephanieanyanwu605_db_user:ciEhwT2C8XhlSyuy@modapazari.jgdqtzw.mongodb.net/?appName=ModaPazari";
 
 mongoose
   .connect(MONGO_URI)
